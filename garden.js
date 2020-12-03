37bb77dbebe1107950deddfa53c2ffc111fca218
Jessica Michel <jessicamichel93@gmail.com>
2020-12-03 15:04:09 +0100

    added comments with working and non working functions

--- garden.js
+++ garden.js
@@ -1,5 +1,20 @@
-const getYieldForPlant = (plant) => plant.yield * 1;
+// const getYieldForPlant = (plant) => plant.yield * 1; // works
+
+const getYieldForPlant = (plant) => {
+    const yieldForPlant = plant.yield * 1; // Cannot read property 'yield' of undefined, but works with above??
+    return yieldForPlant;
+}
+getYieldForPlant();
+
+const getYieldForCrop = (crop) => {
+    const output = crop.yield * crop.input.numCrops;
+    console.log(crop.yield); // Cannot read property 'yield' of undefined
+    console.log('test');
+    return output;
+}
+getYieldForCrop();
 
 module.exports = {
-    getYieldForPlant
+    getYieldForPlant,
+    getYieldForCrop
 };
